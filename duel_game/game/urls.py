ec52918a024ac120d4856af39437ababc151c4c6
Arasid <basa.akrobat@gmail.com>
2018-02-21 23:04:48 +0100

    Login + bootstrap + dummy views.

--- duel_game/game/urls.py
+++ duel_game/game/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+	path('points/', views.points, name='points'),
+	path('add_duel/', views.add_duel, name='add_duel'),
+	path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
 ]
--- duel_game/game/views.py
+++ duel_game/game/views.py
@@ -1,6 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 def index(request):
-    return HttpResponse("DuelGame says hey there world!")
+	return render(request, 'game/index.html', {})
+
+def points(request):
+    return HttpResponse('Here will be points.')
+
+def add_duel(request):
+    return HttpResponse('Here it will be possible to add duels.')
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect('/game/')
+            else:
+                return HttpResponse('Your account is disabled.')
+        else:
+            print('Invalid login details: {0}, {1}'.format(username, password))
+            return HttpResponse('Invalid login details supplied.')
+    else:
+        return render(request, 'game/login.html', {})
+
+
+@login_required(login_url='/game/login/')
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/game/')
 
