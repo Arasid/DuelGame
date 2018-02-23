9e334ef6921ac659ee82e5046e0e44b80849b155
Arasid <basa.akrobat@gmail.com>
2018-02-23 19:51:23 +0100

    Dont allow two people two fingth over and over again.

--- duel_game/game/forms.py
+++ duel_game/game/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from django.db.models import Q
 
-from .models import GameType, Person
+from .models import GameType, Person, Duel
 
 class AddDuelForm(forms.Form):
     game = forms.ModelChoiceField(
@@ -32,7 +33,20 @@ class AddDuelForm(forms.Form):
         cleaned_data = super().clean()
         winner = cleaned_data.get('winner')
         loser = cleaned_data.get('loser')
-        if winner is not None and winner == loser:
+        if None in [winner, loser]:
+            return
+
+        if winner == loser:
             raise forms.ValidationError('Winner and loser cannot be the same.')
-        if None not in [winner, loser] and winner.group == loser.group:
+        if winner.group == loser.group:
             raise forms.ValidationError('Winner and loser are from same group.')
+
+        last_duel_of_winner = Duel.objects.filter(Q(winner=winner) | Q(loser=winner)).last()
+        last_duel_of_loser = Duel.objects.filter(Q(winner=loser) | Q(loser=loser)).last()
+
+        if last_duel_of_winner is not None and loser in [last_duel_of_winner.winner, last_duel_of_winner.loser]:
+            raise forms.ValidationError('%s had his last duel with %s.' % (winner, loser))
+
+        if last_duel_of_loser is not None and winner in [last_duel_of_loser.winner, last_duel_of_loser.loser]:
+            raise forms.ValidationError('%s had his last duel with %s.' % (loser, winner))
+
